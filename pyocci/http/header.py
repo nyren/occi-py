9b55bf3cfe42e8afff703385b96167e44140da92
Ralf Nyren <ralf@nyren.net>
2011-01-25 18:03:28 +0100

    - HTTP header parsing utils

---
+++ pyocci/http/header.py
@@ -0,0 +1,160 @@
+import re
+
+from pyocci.http.utils import escape_quotes, split_quoted
+
+class HttpHeadersBase(object):
+    """Base class for representing a set of HTTP headers with the same name.
+    I.e. support for comma-separated multiple headers.
+
+    >>> h = HttpHeadersBase()
+    >>> h.add('text/plain')
+    >>> h.add('application/xml')
+    >>> h.add('text/html')
+    >>> str(h)
+    'text/plain, application/xml, text/html'
+    >>> h.parse(str(h))
+    ['text/plain', 'application/xml', 'text/html']
+    >>> h.all()
+    ['text/plain', 'application/xml', 'text/html']
+
+    """
+    def __init__(self):
+        self._headers = []
+
+    def add(self, header):
+        self._headers.append(header)
+
+    def parse(self, header_value):
+        self._headers = [self._from_string(value.strip()) for value in split_quoted(header_value)]
+        return self._headers
+
+    def all(self):
+        return self._headers
+
+    def headers(self):
+        return [self._to_string(header) for header in self._headers]
+
+    def _from_string(self, s):
+        return s
+
+    def _to_string(self, header):
+        return header
+
+    def __str__(self):
+        return ', '.join(self.headers())
+
+class HttpAttributeHeaders(HttpHeadersBase):
+    """HTTP Attribute header containing a single key-value pair per header.
+
+    >>> h = HttpAttributeHeaders()
+    >>> h.parse('foo=bar, quote="foo,bar"')
+    [('foo', 'bar'), ('quote', 'foo,bar')]
+    >>> h.add('escape', 'k",inject=something')
+    >>> s = str(h)
+    >>> s
+    'foo="bar", quote="foo,bar", escape="k\\\\",inject=something"'
+    >>> x = h.parse(s)
+    >>> str(h) == s
+    True
+    >>> h.headers() == HttpHeadersBase().parse(s)
+    True
+
+    """
+
+    def add(self, attribute, value):
+        super(HttpAttributeHeaders, self).add((attribute, value))
+
+    def _from_string(self, s):
+        try:
+            k, v = split_quoted(s, delimiter='=', remove_quotes=True)
+        except ValueError:
+            raise HttpHeaderError
+        return (k, v)
+
+    def _to_string(self, header):
+        k, v = header
+        return '%s="%s"' % (k, escape_quotes(str(v)))
+
+class HttpWebHeadersBase(HttpHeadersBase):
+    """Base class for headers like Web Link/Category which have multiple
+    key-value attributes separated by semicolon.
+    """
+    HEADER_VALUE_REGEXP = re.compile(r'^\s*([^;]+)\s*')
+
+    def add(self, item, **attributes):
+        try:
+            attributes['class'] = attributes['class_']
+            del attributes['class_']
+        except KeyError:
+            pass
+        try:
+            attributes['self'] = attributes['self_']
+            del attributes['self_']
+        except KeyError:
+            pass
+        super(HttpWebHeadersBase, self).add((item, attributes))
+
+    def _from_string(self, header_string, value_re=HEADER_VALUE_REGEXP):
+        # Match header value and remove from string
+        m = value_re.match(header_string)
+        if not m:
+            raise HttpHeaderError('%s: invalid header' % header_string)
+        value = m.groups()[0]
+        attr_string = header_string[m.end():]
+
+        # Split and parse string of header attributes
+        attributes = {}
+        for s in split_quoted(attr_string, delimiter=';'):
+            if not s:
+                continue
+            try:
+                k, v = split_quoted(s.strip(), delimiter='=', remove_quotes=True)
+            except ValueError:
+                raise HttpHeaderError("'%s': failed to parse header attribute" % s)
+            attributes[k] = v
+
+        return (value, attributes)
+
+    def _to_string(self, header):
+        item, attributes = header
+        return '; '.join(
+            ['%s' % item] +
+            ['%s="%s"' % (name, escape_quotes(str(value))) for name, value in attributes.iteritems()])
+
+class HttpLinkHeaders(HttpWebHeadersBase):
+    """HTTP Web Link header.
+
+    >>> h = HttpLinkHeaders()
+    >>> h.add('/api/compute/vm01;start', rel='http://purl.org/occi/action#start', class_='action', title='Start')
+    >>> h.add('/api/storage/san1', rel='http://purl.org/occi/kind#storage', class_='link', title='Quorum Disk', device='sda')
+    >>> s = str(h)
+    >>> x = h.parse(s)
+    >>> str(h) == s
+    True
+    >>> h.headers() == HttpHeadersBase().parse(s)
+    True
+
+    """
+    HEADER_VALUE_REGEXP = re.compile(r'^\s*<([^>]+)>\s*')
+
+    def _from_string(self, s):
+        return super(HttpLinkHeaders, self)._from_string(s, value_re=self.HEADER_VALUE_REGEXP)
+
+    def _to_string(self, header):
+        uri, attributes = header
+        return super(HttpLinkHeaders, self)._to_string(("<%s>" % uri, attributes))
+
+class HttpCategoryHeaders(HttpWebHeadersBase):
+    """HTTP Web Category header.
+
+    >>> h = HttpCategoryHeaders()
+    >>> h.add('compute', scheme='http://purl.org/occi/kind#', label='Compute Resource')
+    >>> h.add('ubuntu-9.10', scheme='http://purl.org/occi/category#template', label='Ubuntu Linux 9.10')
+    >>> s = str(h)
+    >>> x = h.parse(s)
+    >>> str(h) == s
+    True
+    >>> h.headers() == HttpHeadersBase().parse(s)
+    True
+    """
+    pass
---
+++ pyocci/http/utils.py
@@ -0,0 +1,51 @@
+def escape_quotes(s, quotechar='"', escapechar='\\'):
+    """Escape quote character and also escape the escape character itself.
+    """
+    buf = ''
+    for c in s:
+        if c == quotechar or c == escapechar:
+            buf += escapechar
+        buf += c
+    return buf
+
+def split_quoted(s, delimiter=',', quotechar='"', escapechar='\\', remove_quotes=False):
+    """Split string on delimiter if and only if delimiter is not within a quote
+    nor escaped by the escape character. An escaped quote character will not
+    affect the quote state during parsing.
+
+    >>> split_quoted(r'name="foo,bar",size=40, key="value",')
+    ['name="foo,bar"', 'size=40', ' key="value"']
+    >>> split_quoted(r'name="foo,bar",size=40, key="value",', remove_quotes=True)
+    ['name=foo,bar', 'size=40', ' key=value']
+    """
+    l = []
+    quote = False
+    escape = False
+    buf = None
+    for c in s:
+        #print "e=%s q=%s\t%s" % (escape, quote, buf)
+        if c == escapechar:
+            escape = not escape
+            if escape and remove_quotes:
+                if buf is None:
+                    buf = ''
+                continue
+        elif c == quotechar and not escape:
+            quote = not quote
+            if remove_quotes:
+                if buf is None:
+                    buf = ''
+                continue
+        elif c == delimiter and not escape and not quote:
+            l.append(buf)
+            buf = None
+            continue
+        elif escape:
+            escape = False
+
+        if buf is None:
+            buf = ''
+        buf += c
+    if buf is not None:
+        l.append(buf)
+    return l
