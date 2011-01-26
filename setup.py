8e2eef9319e765d408991c59091febc31cdca273
Ralf Nyren <ralf@nyren.net>
2011-01-26 15:41:11 +0100

    - packaging stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='occi',
+    version='0.1dev',
+    author='Ralf Nyren',
+    author_email='ralf@nyren.net',
+    packages=['occi',],
+    license='GNU Lesser General Public License (LGPL) version 3',
+    long_description=open('README').read(),
+)
