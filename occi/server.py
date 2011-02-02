ebfde114f6b58641a271a902118be586ac918709
Ralf Nyren <ralf@nyren.net>
2011-02-02 12:17:39 +0100

    - update

--- occi/server.py
+++ occi/server.py
@@ -7,45 +7,22 @@ class OCCIServer(object):
     """An OCCI Server instance."""
 
     def __init__(self, backend):
-        """A Backend object is required to instantiate an OCCI Server."""
-        self._categories = OrderedDict()
+        """A `ServerBackend` object is required to instantiate an OCCI Server."""
+        self.registry = CategoryRegistry()
         self.backend = backend
 
-    def register_category(self, category):
-        """Register a new Category/Kind/Mixin."""
-        s = str(category)
-        if s in self._categories:
-            raise Category.Invalid("%s: Category already defined", s)
-        self._categories[s] = category
-
-    def unregister_category(self, category):
-        """Unregister a previously registered Category/Kind/Mixin."""
-        try:
-            del self._categories[str(category)]
-        except KeyError:
-            raise Category.Invalid("%s: Category not registered", category)
-
-    def lookup_category(self, identifier):
-        try:
-            return self._categories[identifier]
-        except KeyError:
-            raise Category.DoesNotExist
-
-    def lookup_location(self, path):
-        assert(False)
-
-class OCCIBackend(object):
+class ServerBackend(object):
     __metaclass__ = ABCMeta
 
-    def get_entity(self, entitiy_id):
+    def get_entity(self, entitiy_id, user=None):
         pass
-    def filter_entities(self, categories=None, attributes=None, id_prefix=None):
+    def filter_entities(self, categories=None, attributes=None, id_prefix=None, user=None):
         """not too sure about id_prefix filtering, i.e. path"""
         pass
-    def save_entity(self, entity):
+    def save_entity(self, entity, user=None):
         """
         :return entity_id: string
         """
         pass
-    def delete_entity(self, entity):
+    def delete_entity(self, entity, user=None):
         pass
