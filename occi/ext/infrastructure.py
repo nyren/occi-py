1507ef23048986b8b3d96e89e9340eb92c3c53ac
Ralf Nyren <ralf@nyren.net>
2011-02-02 11:46:43 +0100

    - Added default locations

--- occi/ext/infrastructure.py
+++ occi/ext/infrastructure.py
@@ -10,6 +10,7 @@ ComputeKind = Kind('compute', 'http://schemas.ogf.org/occi/infrastructure#',
         title='Compute Resource',
         related=ResourceKind,
         entity_type=Resource,
+        location='compute/',
         attributes=(
             Attribute('occi.compute.architecture', required=False, mutable=False),
             IntAttribute('occi.compute.cores', required=False, mutable=True),
@@ -24,6 +25,7 @@ StorageKind = Kind('storage', 'http://schemas.ogf.org/occi/infrastructure#',
         title='Storage Resource',
         related=ResourceKind,
         entity_type=Resource,
+        location='storage/',
         attributes=(
             FloatAttribute('occi.storage.size', required=True, mutable=True),
             Attribute('occi.storage.state', required=False, mutable=False),
@@ -34,6 +36,7 @@ StorageLinkKind = Kind('storagelink', 'http://schemas.ogf.org/occi/infrastructur
         title='Storage Link', 
         related=LinkKind,
         entity_type=Link,
+        location='link/storage/',
         attributes=(
             Attribute('occi.storagelink.deviceid', required=True, mutable=True),
             Attribute('occi.storagelink.mountpoint', required=False, mutable=True),
