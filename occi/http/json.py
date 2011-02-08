d6447aa4c56b68feb0ee834e9f469bfbfca463a6
Ralf Nyren <ralf@nyren.net>
2011-02-08 21:11:37 +0100

    JSON parser/renderer stub

--- occi/http/json.py
+++ occi/http/json.py
@@ -3,7 +3,10 @@ from occi.http.parser import Parser
 from occi.http.renderer import Renderer
 
 class JSONParser(Parser):
-    pass
+    """Parser for the application/json content type."""
+    raise NotImplemented('yet')
 
 class JSONRenderer(Renderer):
+    """Renderer for the application/json content type."""
+    raise NotImplemented('yet')
     pass
