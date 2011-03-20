46ba18346bd6fe6d9b5b0e779778ac649f8a72ad
Ralf Nyren <ralf@nyren.net>
2011-03-20 15:22:40 +0100

    Simple action handling for Compute.

--- occi/demo/iaas_demo.py
+++ occi/demo/iaas_demo.py
@@ -21,6 +21,18 @@ class Compute(occi.core.Resource):
 
         self.occi_set_applicable_action(ComputeStartActionCategory)
 
+    def exec_action(self, action, payload=None):
+        state = self.get_occi_attribute('occi.compute.state')
+        if state == 'inactive' and action.category.term == 'start':
+            self.set_occi_attributes([('occi.compute.state', 'active')], validate=False)
+            self.occi_set_applicable_action(ComputeStartActionCategory, applicable=False)
+            self.occi_set_applicable_action(ComputeStopActionCategory, applicable=True)
+        elif state == 'active' and action.category.term == 'stop':
+            self.set_occi_attributes([('occi.compute.state', 'inactive')], validate=False)
+            self.occi_set_applicable_action(ComputeStopActionCategory, applicable=False)
+            self.occi_set_applicable_action(ComputeStartActionCategory, applicable=True)
+        return None
+
 def init_server(backend):
     ComputeKind.entity_type = Compute
 
