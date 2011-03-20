08ce5732effbc7e5e980f2af222ed6de19ebc2b6
Ralf Nyren <ralf@nyren.net>
2011-03-20 12:27:37 +0100

    Added custom Compute entity_type with defaults.

--- occi/demo/iaas_demo.py
+++ occi/demo/iaas_demo.py
@@ -3,11 +3,27 @@
 import argparse
 import urlparse
 
+import occi.core
 from occi.server import OCCIServer, DummyBackend
 from occi.ext.infrastructure import *
 from occi.http.tornado_frontend import TornadoHttpServer
 
+class Compute(occi.core.Resource):
+    def __init__(self, kind, **kwargs):
+        super(Compute, self).__init__(kind, **kwargs)
+        attr_default = [
+                ('occi.compute.architecture', 'x86_64'),
+                ('occi.compute.speed', 2.67),
+                ('occi.compute.memory', 1.0),
+                ('occi.compute.state', 'inactive'),
+        ]
+        self.set_occi_attributes(attr_default, validate=False)
+
+        self.occi_set_applicable_action(ComputeStartActionCategory)
+
 def init_server(backend):
+    ComputeKind.entity_type = Compute
+
     server = OCCIServer(backend=backend)
     server.registry.register(ComputeKind)
     server.registry.register(NetworkKind)
